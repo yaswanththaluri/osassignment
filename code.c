5a7c745cefa8a05a9ba7ad7f1855a49e240d5f9e
T B Yaswanth Kumar <36106569+yaswanththaluri@users.noreply.github.com>
2018-04-07 11:37:06 +0530

    Update code.c

--- code.c
+++ code.c
@@ -3,20 +3,41 @@
 #include<unistd.h>
 #define MIN_PID 100
 #define MAX_PID 1000
+#define array_size MAX_PID - MIN_PID
+
+
+int main()
+{
+	allocate_map();
+	printf("\n After allocation\n");
+	display();
+	
+}
 
 int allocate_map()
 {
-
+	int i;
+	for(i = 0; i < array_size; i++)
+	{
+		status_of_pid[i] = 0;
+	}
 }
 
 int allocate_pid()
 {
-
+	int i;
+	for(i = 0; i < array_size; i++)
+	{
+		if( status_of_pid[i] == 0)
+		{
+			status_of_pid[i] == 1;
+			return i+100;
+		}
+	}
 }
 
 void release_pid()
 {
-
+	
 }
-
 
