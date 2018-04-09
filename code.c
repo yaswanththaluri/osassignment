f232a82f8738cf6f7fc6891bedcb5883a85dc06d
T B Yaswanth Kumar <36106569+yaswanththaluri@users.noreply.github.com>
2018-04-09 19:57:09 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,132 +1,114 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
 #include<time.h>
+#include<unistd.h>
+
 #define MIN_PID 100
 #define MAX_PID 1000
-#define ARR_SIZE MAX_PID-MIN_PID+1
+#define array_size MAX_PID-MIN_PID+1
 
-int pid_status[ARR_SIZE];
+void *thread_fun(void *args);
 
-int allocate_map()        
+int processID_status[ array_size ];
+
+struct process
 {
-    int i;
-    for(i=0;i<ARR_SIZE;i++)
-    {
-        pid_status[i]=0;
-    }
-    return 0;               
-}
-
-int allocate_pid()
-{
-    int i;
-    for(i=0;i<ARR_SIZE;i++)
-    {
-        if(pid_status[i]==0)   
-        {
-            pid_status[i]=1;     
-            return i+100;        
-        }
-    }
-    return 1;                
-}
-
-void release_pid(int p)
-{
-  int pid=p-100;            
-  pid_status[pid]=0;       
-}
-
-void display()
-{
-  int i;
-  for(i=0;i<ARR_SIZE;i++)
-  {
-    printf("[%d] = %d\t",i+100,pid_status[i]);
-  }
-}
-
-
-struct proc
-{
-	int sno;
-    int pid;
-    int sl_time;
+int process_number;
+int pid;
+int time;
 };
 
-int pid_check(int pid)
-{
-    if(pid>=100)                        
-    {
-        printf("\nPid %d allocated successfully...\n",pid);
-        return 0;                               
-    }
-    else if(pid==1)                     
-    {
-        printf("\nAllocating pid has been failed...\n"); 
-        return 1;                        
-    }
-    else                        
-    {
-        printf("\nError.... pid: %d is returned...",pid);
-        return 1;
-    }
-}
-
-int set_sleep_time()
-{
-    return rand()%10;                     
-}
-
-void *thread_fun(void *pro)              
-{
-    struct process *p = (struct process *)pro;
-    printf("\n%d: Fetching pid...\n",p->sno);
-    p->pid = allocate_pid();               
-  
-    while(pid_check(p->pid)==1)             
-    {
-        sleep(5);                            
-        printf("\n%d: Fetching pid...\n",p->sno);   
-        p->pid = allocate_pid();             
-    }
-    display();                           
-    p->sl_time = set_sleep_time();         
-    printf("\n%d: pid = %d,sl_time = %d\n",p->sno,p->pid,p->sl_time);
-    printf("\n%d - %d: Entered Sleep time...\n",p->sno,p->pid);
-    sleep(p->sl_time);                                                  
-    printf("\n%d - %d: Exited Sleep time...\n",p->sno,p->pid);
-    release_pid(p->pid);                                
-    display();
-}
 
 int main()
 {
-    int i, no_of_threads;
-    printf("Enter no of threads: ");
-    scanf("%d",&no_of_threads);               
-    printf("...program gonna start...\n");
-  
-    int x = allocate_map();                  
-    if(x==0)                                  
-        printf("Mapping allocated...\n");
-    else
-        printf("Problem in initializing statuses...\n");
-  
-    struct process pr[no_of_threads];                    
-    pthread_t pr_threads[no_of_threads];              
-  
-    for(i=0;i<no_of_threads;i++)
-    {
-        pr[i].sno = i;                                 
-        pthread_create(&pr_threads[i],NULL,thread_fun,&pr[i]);          
-    }
-  
-    for(i=0;i<no_of_threads;i++)              
-    {
-        pthread_join(pr_threads[i],NULL);                    
-    }
-  
-    printf("\n...Program terminated successfully...\n");
+int no_of_processes;
+printf("Enter number of Threads: \n");
+scanf("%d",&no_of_processes);
+
+int result = allocation_map();
+
+if(result == 0)
+{
+printf("Map Allocated successfully");
+}
+else
+{
+printf("Error occured in Allocation Map");
+}
+
+
+struct process p[no_of_processes];
+pthread_t process_threads[no_of_processes];
+
+for(int i=0; i<no_of_processes; i++)
+{
+p[i].process_number = i;
+pthread_create(&process_threads[i], NULL, thread_fun, &p[i]);
+}
+
+for(int i=0; i<no_of_processes; i++)
+{
+pthread_join(process_threads[i],NULL);
+}
+}
+
+
+
+int allocation_map()
+{
+int i;
+for(i = 0; i < array_size; i++)
+{
+processID_status[i] = 0;
+}
+return 0;
+}
+void *thread_fun(void *pro)
+{
+struct process *p = (struct process*)pro;
+printf("%d is fetching process id\n",p->process_number);
+p->pid = allocate_pid();
+while(processid_check(p->pid) == 1)
+{
+sleep(2);
+printf("\n%d is fetching pid...\n",p->process_number);
+p->pid = allocate_pid();
+}
+}
+
+
+int allocate_pid()
+{
+int i;
+for(i = 0;i<array_size;i++)
+{
+if(processID_status[i] == 0)
+{
+processID_status[i] = 1;
+return i+100;
+	}
+}
+return 1;
+}
+
+
+int processid_check(int pid)
+{
+if(pid>=100)
+{
+printf("Process ID: %d allocated to the process Successfully\n",pid);
+return 0;
+}
+else if(pid == 1)
+{
+printf("Allocating Process ID has been Failed\n");
+return 1;
+}
+else
+{
+printf("Error.........\n");
+return 1;
+}
 }
